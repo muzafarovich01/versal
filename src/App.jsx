d7a777a36abd9308925116799d1489714bc5dcb9
muzafarovich01 <sarvark4636363@gmail.com>
2025-12-11 15:34:52 +0500

    sarvar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus, unde?</h1>
+    </>
+  )
+}
+
+export default App
