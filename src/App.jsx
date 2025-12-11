f5a2e82c58e13415a0dc12cfd39d22c4313635c0
muzafarovich01 <sarvark4636363@gmail.com>
2025-12-11 15:39:13 +0500

    h2

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <>
       <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus, unde?</h1>
+      <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, eos accusantium iste repellendus voluptatum repudiandae corrupti explicabo fugit quibusdam magnam officiis dolor, molestias exercitationem tempora modi esse. Eum quam quo temporibus. Maiores velit libero, doloremque ut necessitatibus rerum mollitia, dolor officiis hic unde molestiae modi dolorum. Veniam deleniti recusandae aspernatur?</h2>
     </>
   )
 }
